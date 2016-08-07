3d59502c2e8ec5c4b1909966e45d48aaf63ea50a
Bogdan Niculeasa <bogdan.niculeasa@hotmail.com>
2016-08-08 00:59:15 +0300

    =created functionality to check if a certain file is a valid assembly

---
+++ AssemblyConfigurationDetector/AssemblyValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+
+  namespace AssemblyConfigurationDetector
+{
+    public class AssemblyValidator
+    {
+        public bool IsValid(string assemblyPath)
+        {
+            try
+            {
+                AssemblyName.GetAssemblyName(assemblyPath);
+            }
+            catch (BadImageFormatException ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ AssemblyConfigurationDetectorTest/AssemblyValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using AssemblyConfigurationDetector;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AssemblyConfigurationDetectorTest
+{
+    [TestClass]
+    public class AssemblyValidatorTest
+    {
+        [TestMethod]
+        [DeploymentItem("TestFiles")]
+        public void XmlAsAssembly_IsValid_ReturnsFalse()
+        {
+            AssemblyValidator assemblyValidator = new AssemblyValidator();
+
+            bool actual = assemblyValidator.IsValid("XmlAsDll.dll");
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestFiles")]
+        public void ValidAssembly_IsValid_ReturnsTrue()
+        {
+            AssemblyValidator assemblyValidator = new AssemblyValidator();
+
+            bool actual = assemblyValidator.IsValid("ValidAssembly.dll");
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DeploymentItem("TestFiles")]
+        public void ValidAssembly_IsValidMultipleCalls_NoExceptionThrown()
+        {
+            AssemblyValidator assemblyValidator = new AssemblyValidator();
+
+            assemblyValidator.IsValid("ValidAssembly.dll");
+            assemblyValidator.IsValid("ValidAssembly.dll");
+            assemblyValidator.IsValid("ValidAssembly.dll");
+            assemblyValidator.IsValid("ValidAssembly.dll");
+      
+        }
+    }
+}
